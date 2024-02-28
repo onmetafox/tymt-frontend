0f589e5e4d5eb6824b0b1baaf6035e2813cabc14
onmetafox <onemetafox@outlook.com>
2024-02-28 12:17:36 +0700

    fix typo

--- src/view/Home.js
+++ src/view/Home.js
@@ -3,8 +3,8 @@ import { Tooltip } from 'react-tooltip'
 // import "../styles/styles.css";
 import "../styles/font.css";
 import "../styles/colors.css";
-import solarImage from "../assets/images/temp-img.svg";
-import tempImg from "../assets/images/temp-img-sm.svg";
+import blueImg from "../assets/images/temp-blue.svg";
+import foxImg from "../assets/images/temp-fox.svg";
 import Download from "../components/DownCmp";
 import winIco from "../assets/icons/win-icon.svg";
 import linuxIco from "../assets/icons/linux-icon.svg";
@@ -31,8 +31,8 @@ const Home = () => {
         <section id="home" className="home home-section">
             <div className='mobile-bg'></div>
                 <div className="container ">
-                <div className="about-right-light blink"></div>
-                <div className="about-left-light blink delayed-animation"></div>
+                <div className="home-left-light blink"></div>
+                <div className="home-right-light blink delayed-animation"></div>
                 {!mobileView && (
                 <div className={`about-title-layout`}>
                     <div className="about-title">
@@ -57,26 +57,42 @@ const Home = () => {
                         <p className='fs-p grey-color'>
                             If you are a developer and want to create or publish a game using the benefits of the Solar blockchain - check out our GitHub documentation.
                         </p>
-                        <div className="action-button header-button fc-l">
+                        <a className="action-button header-button fc-l" href = 'https://github.com/solar-network' target='_blank' rel="noreferrer">
                             <span>Developer GitHub</span>
-                        </div>
+                        </a>
                     </div>
                     <div className="about-solar-logo">
-                        <img className="solar-img" alt="solar logo" src={solarImage} />
+                        <div className='solar-container'>
+                            <img className="solar-img" alt="solar logo" src={blueImg} />
+                            <img className='fox-img' alt='fox' src={foxImg}/>
+                        </div>
+                        
                         <div className='support-container'>
                             <span className='grey-color fc-m support-text'>Supported by:</span>
                             <div className='os-container'>
                                 <div className='os-item'>
-                                    <img src= {winIco} width={24} alt='win icon'/>
-                                    <span className='fc-m white-color'>Windows</span>
+                                    <div>
+                                        <img src= {winIco} alt='win icon'/>
+                                    </div>
+                                    <div className='fc-m white-color'>
+                                        Windows
+                                    </div>
                                 </div>
                                 <div className='os-item'>
-                                    <img src= {linuxIco} width={24} alt='linux icon'/>
-                                    <span className='fc-m white-color'>Linux</span>
+                                    <div>
+                                        <img src= {linuxIco} alt='win icon'/>
+                                    </div>
+                                    <div className='fc-m white-color'>
+                                        Linux
+                                    </div>
                                 </div>
                                 <div className='os-item' data-tooltip-id="my-tooltip-inline" data-tooltip-content="Coming Soon...">
-                                    <img src= {macIco} width={24} alt='mac icon'/>
-                                    <span className='fc-m white-color'>MacOS</span>
+                                    <div>
+                                        <img src= {macIco} alt='win icon'/>
+                                    </div>
+                                    <div className='fc-m white-color'>
+                                        MacOS
+                                    </div>
                                     <Tooltip  id="my-tooltip-inline" className='tooltiptext fc-m' style={{borderRadius: '30px'}}/>
                                 </div>
                             </div>
@@ -98,7 +114,7 @@ const Home = () => {
                         </p>
                         <div className="dropdown">
                             <div className="action-button download-button fc-l">
-                                <span>Install and Play now</span>
+                                <span>Not support on Mobile</span>
                             </div>
                             <div className="dropdown-content">
                                 <Download />
@@ -106,29 +122,44 @@ const Home = () => {
                         </div> 
                     </div>
                     <div className="about-solar-logo">
-                        <img alt="solar logo" src={tempImg} />
+                        <div className='solar-container'>
+                            <img className="solar-img" alt="solar logo" src={blueImg} />
+                            <img className='fox-img' alt='fox' src={foxImg}/>
+                        </div>
                     </div>
                     <p className='fs-p grey-color developer-text'>
                         If you are a developer and want to create or publish a game using the benefits of the Solar blockchain - check out our GitHub documentation.
                     </p>
-                    <div className="action-button header-button fc-l">
+                    <a className="action-button header-button fc-l" href = 'https://github.com/solar-network' target='_blank' rel="noreferrer">
                         <span>Developer GitHub</span>
-                    </div>
+                    </a>
                     <div className='support-container'>
                         <span className='grey-color fc-m support-text'>Supported by:</span>
                         <div className='os-container'>
                             <div className='os-item'>
-                                <img src= {winIco} width={19} alt='win icon'/>
-                                <span className='fc-m white-color'>Windows</span>
+                                <div>
+                                    <img src= {winIco} alt='win icon'/>
+                                </div>
+                                <div className='fc-m white-color'>
+                                    Windows
+                                </div>
                             </div>
                             <div className='os-item'>
-                                <img src= {linuxIco} width={19} alt='linux icon'/>
-                                <span className='fc-m white-color'>Linux</span>
+                                <div>
+                                    <img src= {linuxIco} alt='win icon'/>
+                                </div>
+                                <div className='fc-m white-color'>
+                                    Linux
+                                </div>
                             </div>
-                            <div className='os-item tooltip'>
-                                <img src= {macIco} width={19} alt='mac icon'/>
-                                <span className='fc-m white-color'>MacOS</span>
-                                <span className="tooltiptext fc-m">Coming Soon</span>
+                            <div className='os-item' data-tooltip-id="my-tooltip-inline" data-tooltip-content="Coming Soon...">
+                                <div>
+                                    <img src= {macIco} alt='win icon'/>
+                                </div>
+                                <div className='fc-m white-color'>
+                                    MacOS
+                                </div>
+                                <Tooltip  id="my-tooltip-inline" className='tooltiptext fc-m' style={{borderRadius: '30px'}}/>
                             </div>
                         </div>
                     </div>
