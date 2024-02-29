9db94c5643401c9e1ec5bfe447881cc3d71c1b48
onmetafox <onemetafox@outlook.com>
2024-02-29 12:12:38 +0100

    button text align resolved

--- src/view/Footer.js
+++ src/view/Footer.js
@@ -15,10 +15,10 @@ const Footer = () => {
       <div className="container">
         <div className="footer-layout">
           <div className="footer-copyright color-gray">
-            <p className="fc-m grey-color" style={{whiteSpace: "nowrap"}}>©2024, All right reserved.</p>
+            <p className="fc-m grey-color" style={{whiteSpace: "nowrap"}}>©2024. All rights reserved.</p>
             <div className="fc-m">
               <p className="fc-m hover-gray-color grey-color footer-right">
-                The first game available on tymt is District 53 - a voxel-based game, based on the Mintetest engine.<br/>
+                The first game available on tymt is District 53 - a voxel-based game, based on the Minetest engine.<br/>
                 It’s free to access, free for all, free to play, free to build - AND players can earn rewards while playing it!
               </p>
             </div>
--- src/view/Home.js
+++ src/view/Home.js
@@ -9,6 +9,7 @@ import Download from "../components/DownCmp";
 import winIco from "../assets/icons/win-icon.svg";
 import linuxIco from "../assets/icons/linux-icon.svg";
 import macIco from "../assets/icons/mac-icon.svg";
+import macIcoGray from "../assets/icons/mac-icon-gray.svg";
 const Home = () => {
     function changeView () {
         const { innerWidth: width, innerHeight: height } = window;
@@ -31,11 +32,10 @@ const Home = () => {
         <section id="home" className="home home-section">
             <div className='mobile-bg'></div>
                 <div className="container ">
-                <div className="home-left-light blink"></div>
-                <div className="home-right-light blink delayed-animation"></div>
+                
                 {!mobileView && (
-                <div className={`about-title-layout`}>
-                    <div className="about-title">
+                <div className={`about-title-layout row`}>
+                    <div className="about-title col-lg-6 col-md-6">
                         <div className="color-about-title">
                             <span className="h2-italic white-color">Ready to</span>
                             <br />
@@ -57,11 +57,11 @@ const Home = () => {
                         <p className='fs-p grey-color'>
                             If you are a developer and want to create or publish a game using the benefits of the Solar blockchain - check out our GitHub documentation.
                         </p>
-                        <a className="action-button header-button fc-l" href = 'https://github.com/solar-network' target='_blank' rel="noreferrer">
+                        <a className="action-button header-button fc-l" href = 'https://github.com/dokdo-sh/tymtLauncher' target='_blank' rel="noreferrer">
                             <span>Developer GitHub</span>
                         </a>
                     </div>
-                    <div className="about-solar-logo">
+                    <div className="about-solar-logo col-lg-6 col-md-6">
                         <div className='solar-container'>
                             <img className="solar-img" alt="solar logo" src={blueImg} />
                             <img className='fox-img' alt='fox' src={foxImg}/>
@@ -86,11 +86,11 @@ const Home = () => {
                                         Linux
                                     </div>
                                 </div>
-                                <div className='os-item' data-tooltip-id="my-tooltip-inline" data-tooltip-content="Coming Soon...">
+                                <div className='os-item' data-tooltip-id="my-tooltip-inline" data-tooltip-content="coming soon">
                                     <div>
-                                        <img src= {macIco} alt='win icon'/>
+                                        <img src= {macIcoGray} alt='win icon'/>
                                     </div>
-                                    <div className='fc-m white-color'>
+                                    <div className='fc-m nav-color'>
                                         MacOS
                                     </div>
                                     <Tooltip  id="my-tooltip-inline" className='tooltiptext fc-m' style={{borderRadius: '30px'}}/>
@@ -112,14 +112,17 @@ const Home = () => {
                         <p className='fs-p white-color'>
                             We are developing the next generation Game Launcher that bridges the gaps between blockchain and traditional gaming.
                         </p>
-                        <div className="dropdown">
-                            <div className="action-button download-button fc-l">
-                                <span>Not support on Mobile</span>
+                        <p className='fs-p white-color'>
+                            
+                        </p>
+                        {/* <div className="dropdown">
+                            <div className="action-button download-button fc-l modal-button-disable">
+                                <span></span>
                             </div>
                             <div className="dropdown-content">
                                 <Download />
                             </div>
-                        </div> 
+                        </div>  */}
                     </div>
                     <div className="about-solar-logo">
                         <div className='solar-container'>
@@ -130,7 +133,7 @@ const Home = () => {
                     <p className='fs-p grey-color developer-text'>
                         If you are a developer and want to create or publish a game using the benefits of the Solar blockchain - check out our GitHub documentation.
                     </p>
-                    <a className="action-button header-button fc-l" href = 'https://github.com/solar-network' target='_blank' rel="noreferrer">
+                    <a className="action-button header-button fc-l" href = 'https://github.com/dokdo-sh/tymtLauncher' target='_blank' rel="noreferrer">
                         <span>Developer GitHub</span>
                     </a>
                     <div className='support-container'>
@@ -154,12 +157,12 @@ const Home = () => {
                             </div>
                             <div className='os-item' data-tooltip-id="my-tooltip-inline" data-tooltip-content="Coming Soon...">
                                 <div>
-                                    <img src= {macIco} alt='win icon'/>
+                                    <img src= {macIcoGray} alt='win icon'/>
                                 </div>
-                                <div className='fc-m white-color'>
+                                <div className='fc-m nav-color'>
                                     MacOS
                                 </div>
-                                <Tooltip  id="my-tooltip-inline" className='tooltiptext fc-m' style={{borderRadius: '30px'}}/>
+                                <Tooltip id="my-tooltip-inline" isOpen={true} className='tooltiptext fc-m' place={'bottom-start'} style={{ borderRadius: '30px'}} />
                             </div>
                         </div>
                     </div>
