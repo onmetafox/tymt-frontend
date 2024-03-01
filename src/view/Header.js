6f525f9c6373ecd68f93efabfe268ddf91b9793a
onmetafox <onemetafox@outlook.com>
2024-02-29 20:24:53 -0800

    fix: button align center and wrap issues fixed

--- src/view/Header.js
+++ src/view/Header.js
@@ -4,10 +4,13 @@ import Download from "../components/DownCmp";
 import logo from "../assets/logos/logo-transp.png";
 // import available from "../assets/images/availe.svg";
 import available_mobile from "../assets/images/available-mobile.svg";
-
+import { OS, currentBrowser } from '../utils/getEnv';
+import { current } from 'immer';
 
 const Header = () =>{
     const navigate = useNavigate();
+    const [os, setOs] = useState(OS(window));
+    const [browser, setBrowser] = useState(currentBrowser(window));
     function changeView () {
         const { innerWidth: width, innerHeight: height } = window;
         if(width <= 600 && height){
@@ -18,6 +21,12 @@ const Header = () =>{
         
     }
     const [mobileView, setMobileView] = useState(changeView());
+
+    useEffect(() => {
+        console.log("os", os);
+        console.log("browser", browser);
+    }, [os, browser])
+
     useEffect(() => {
         function handleResize() {
             setMobileView(changeView());
@@ -26,17 +35,18 @@ const Header = () =>{
         return () => window.removeEventListener('resize', handleResize);
     }, []);
     return(
+        
         <header className="header">
             <div className='row'>
                 <div className="header-logo col-lg-7 col-md-6 col-sm-6" onClick={() => {navigate("/")}}>
                     <img src= {logo} className='header-logo-image' alt='logo'/>
                 </div>
                 <div className='header-button-group col-lg-5 col-md-6 col-sm-6'>
-                    <a className="action-button header-button fc-l" href = 'https://github.com/dokdo-sh/tymtLauncher' target='_blank' rel="noreferrer">
+                    <a className={OS === "Windows OS" ? "action-button header-button-windows fc-l" : "action-button header-button fc-l"} href='https://github.com/dokdo-sh/tymtLauncher' target='_blank' rel="noreferrer">
                         <span>Developer GitHub</span>
                     </a>
                     <div className="dropdown">
-                        {!mobileView && <div className="action-button download-button fc-l">    
+                        {!mobileView && <div className={OS === "Windows OS" ? "action-button download-button-windows fc-l":"action-button download-button fc-l"}>    
                             Install and Play now
                         </div>}
                         {/* {mobileView && <div className="download-button fc-l modal-button-disable">    
