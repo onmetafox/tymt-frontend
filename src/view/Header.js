dc92809e3dbaec86ed042c8e3078d255776bb700
onmetafox <onemetafox@outlook.com>
2024-02-29 21:24:50 -0800

    fix: text align issue on OS

--- src/view/Header.js
+++ src/view/Header.js
@@ -1,19 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState} from 'react';
 import { useNavigate } from "react-router-dom";
 import { isMobile } from 'react-device-detect';
 import Download from "../components/DownCmp";
 import logo from "../assets/logos/logo-transp.png";
 // import available from "../assets/images/availe.svg";
 import available_mobile from "../assets/images/available-mobile.svg";
-import { OS, currentBrowser } from '../utils/getEnv';
+import { OS } from '../utils/getEnv';
 
 const Header = () =>{
     const navigate = useNavigate();
-    const [os, setOs] = useState(OS(window));
-    const [browser, setBrowser] = useState(currentBrowser(window));
-
-    useEffect(() => {
-    }, [os, browser])
+    const [os] = useState(OS(window));
     
     return(
         
@@ -23,11 +19,11 @@ const Header = () =>{
                     <img src= {logo} className='header-logo-image' alt='logo'/>
                 </div>
                 <div className='header-button-group col-lg-5 col-md-6 col-sm-6'>
-                    <a className={OS === "Windows OS" ? "action-button header-button-windows fc-l" : "action-button header-button fc-l"} href='https://github.com/dokdo-sh/tymtLauncher' target='_blank' rel="noreferrer">
+                    <a className={os === "Windows OS" ? "action-button header-button-windows fc-l" : "action-button header-button fc-l"} href='https://github.com/dokdo-sh/tymtLauncher' target='_blank' rel="noreferrer">
                         <span>Developer GitHub</span>
                     </a>
                     <div className="dropdown">
-                        {!isMobile && <div className={OS === "Windows OS" ? "action-button download-button-windows fc-l":"action-button download-button fc-l"}>    
+                        {!isMobile && <div className={os === "Windows OS" ? "action-button download-button-windows fc-l":"action-button download-button fc-l"}>    
                             Install and Play now
                         </div>}
                         {/* {mobileView && <div className="download-button fc-l modal-button-disable">    
