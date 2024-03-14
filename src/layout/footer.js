b92b2d55020f45e54dd60e0407ccd6629965534c
onmetafox <onemetafox@outlook.com>
2024-03-14 01:34:58 -0700

    fix: fixed small issues

--- src/layout/footer.js
+++ src/layout/footer.js
@@ -13,17 +13,17 @@ const Footer = () => {
                     <Link to="how-to-install" smooth={true} duration={500} className='fs-16 white m-lr-10'>How it Works</Link>
                     <Link to="featured" smooth={true} duration={500} className='fs-16 white m-lr-10'>Publishers</Link>
                 </div>
-                <div className="col-lg-4 col-md-4 col-sm-12 content-center twitter-logo">
+                <a className="col-lg-4 col-md-4 col-sm-12 content-center twitter-logo" href="https://twitter.com/SolarNetwork" target="_blank" rel="noreferrer">
                     <img src={twiterIcon} alt="twitter"/>
-                </div>
+                </a>
             </div>
             <div className="row m-t-20">
                 <div className="col-lg-6 col-md-6 col-sm-12 fs-16 gray footer-right content-center">
-                    ©2024, All right reserved.
+                    ©2024. All right reserved.
                 </div>
                 <div className="col-lg-6 col-md-6 col-sm-12 d-flex content-center footer-terms">
-                    <div className="fs-16 gray m-lr-10">Privacy Policy</div>
-                    <div className="fs-16 gray">Terms of Service</div>
+                    <a href = "https://solar.network/terms-and-conditions" target="_blank" rel="noreferrer"  className="fs-16 gray m-lr-10">Privacy Policy</a>
+                    <a href = "https://solar.network/terms-and-conditions" target="_blank" rel="noreferrer" className="fs-16 gray">Terms of Service</a>
                 </div>
             </div>
         </div>
